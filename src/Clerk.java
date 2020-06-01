a4ee6233290964dcdacb427ce49b29dffde21512
yuligros <64066535+yuligros@users.noreply.github.com>
2020-06-01 17:15:46 +0300

    Update Clerk.java

--- src/Clerk.java
+++ src/Clerk.java
@@ -12,35 +12,47 @@ public class Clerk extends Thread {
 		this.callLine = callLine;
 	}
 
-	public void run() {
+	public synchronized void run() {
+		while(callLine.isEmpty()) {
+			try {
+				this.wait();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		notifyAll();
 		Call c = callLine.extract();
+		System.out.println("yuli");
 		salary+=2;
-		try {
-			Thread.sleep((long) (c.getCallDuration() * 1000));
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if (c.getNumOfPizzas() < 10) {
-			Order o = createOrder(c);
-			orders.insert(o);
-			try {
-				join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} 
-		else  {
-			try {
-				sleep(500);// need to transfer to manager call line
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			managerLine.insert(c);	
-		}
+//		try {
+//			Thread.sleep((long) (c.getCallDuration() * 1000));
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		if (c.getNumOfPizzas() < 10) {
+//			Order o = createOrder(c);
+//			orders.insert(o);
+//			try {
+//				join();
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		} 
+//		else  {
+//			try {
+//				sleep(500);// need to transfer to manager call line
+//				
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			managerLine.insert(c);	
+//		}
+		
+		
 
 	}
 	
